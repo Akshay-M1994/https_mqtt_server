2acbabe150ad48af22795049083b3c600bb5b547
Akshay-M1994 <Akshaymaharaj7@gmail.com>
2024-02-20 09:27:02 +0200

    Added help route

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
 from flask import Flask
+import json
+
+#Flask http host server
+FLASK_HTTP_SERVER = "0.0.0.0"
+FLASK_HTTP_PORT   = 4000
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, World!"
-
+@app.route("/help")
+def help():
+    data = {"routeList":
+                [
+                    {
+                     "routeName":"help",
+                     "requestURL":"http://serverAddress:serverPort/profileList",
+                     "description":"Returns a list of profiles of different modbus devices current present on the modbus"
+                    },
+                    {
+                     "routeName":"getAPIs",
+                     "requestURL":"http://serverAddress:serverPort/getAPIs",
+                     "Request Body(Json)":{"deviceType":"DeviceType"},
+                     "description":"Returns a list of commands that can be sent to a particular modbus device"
+                    },
+                    {
+                     "routeName":"profileList",
+                     "requestURL":"http://serverAddress:serverPort/profileList",
+                     "description":"Returns a list of profiles of different modbus devices current present on the modbus"
+                    },
+                    {
+                     "routeName":"profileList",
+                     "requestURL":"http://serverAddress:serverPort/profileList",
+                     "description":"Returns a list of profiles of different modbus devices current present on the modbus"
+                    }
+                ]
+        }
+    return json.dumps(data)
+    
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(FLASK_HTTP_SERVER, FLASK_HTTP_PORT, debug=True)
